bcc6f4cf07738d0e144c39e1103f70164a621341
Sheheryar Waseem <sherishaikh6@gmail.com>
2021-11-15 21:37:28 +0500

    updated

--- app.js
+++ app.js
@@ -67,8 +67,7 @@ function editUser(_id, index) {
 
   console.log("userObject: ", userObject);
 
-  document.getElementById(_id).innerHTML = `
-  <tr id="${_id}"> 
+  document.getElementById(_id).innerHTML = `<tr id="${_id}"> 
       
           <th scope="row">${_id}</th>
           <td><input type="text" id="${_id}-name" value="${userObject.name}" /></td>
@@ -79,3 +78,31 @@ function editUser(_id, index) {
           </td>
       </tr>`;
 }
+
+function updateUser(_id) {
+
+  const name = document.getElementById(`${_id}-name`).value
+  const email = document.getElementById(`${_id}-email`).value
+  const address = document.getElementById(`${_id}-address`).value
+
+  axios.put(`https://registration-assignment1.herokuapp.com/user/${_id}`, { name, email, address })
+      .then(function (response) {
+          console.log(response);
+
+          getAllUser();
+
+          document.getElementById("alert").innerHTML =
+              `<div class="alert alert-success" role="alert">
+                  User Updated Success!
+              </div>`
+
+          setTimeout(() => {
+              document.getElementById("alert").innerHTML = ""
+          }, 3000);
+
+      })
+}
+
+
+
+getAllUser();
